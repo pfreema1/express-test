d557f6eab0c510c8bbddc235b66b022073dcce76
pfreema1 <paul@level2d.com>
2021-04-04 16:47:37 -0400

    yay

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello Express');
+  //   res.send('Hello Express');
+  const path = __dirname + '/views/index.html';
+  res.sendFile(path);
 });
 
 module.exports = app;
