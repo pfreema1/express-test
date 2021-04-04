54605530969acb97ea35faf55e5a754934f4773c
pfreema1 <paul@level2d.com>
2021-04-04 16:37:18 -0400

    yay

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello sadfsaWorld!');
+  res.send('Hello World!');
 });
+
+console.log('Hello World');
 
 module.exports = app;
