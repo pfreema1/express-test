7621d2e38c560b7bce9ed5978ceb432c831751c4
pfreema1 <paul@level2d.com>
2021-04-04 16:58:46 -0400

    yay

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 // middleware
-app.use('/public', __dirname + '/public');
+app.use(__dirname + '/public');
 
 app.get('/', function(req, res) {
   //   res.send('Hello Express');
